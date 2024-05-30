c876a12456f0346f4ba32c6ef086db6525e9d650
Gido <120196095+gidopa@users.noreply.github.com>
2024-05-30 17:34:01 +0900

    add: 더미 데이터 삽입

--- src/main/java/Team/project/InitData.java
+++ src/main/java/Team/project/InitData.java
@@ -33,13 +33,18 @@ public class InitData {
         public void init(){
             Major computer = new Major("computer", new ArrayList<>());
             Professor p1 = new Professor("p1", computer);
+            Professor p2 = new Professor("p2", computer);
             p1.changeMajor(computer);
+            p2.changeMajor(computer);
             Course java = new Course("JAVA", p1, 0.3, 0.3, 0.4, 3, Category.MAJOR);
+            Course AI = new Course("AI", p1, 0.3, 0.3, 0.4, 3, Category.MAJOR);
 
 
             em.persist(computer);
             em.persist(p1);
             em.persist(java);
+            em.persist(p2);
+            em.persist(AI);
             List<Enroll> enrolls = new ArrayList<>();
 
             for (int i = 1; i < 20; i++) {
@@ -53,6 +58,20 @@ public class InitData {
                 em.persist(enroll);
                 em.persist(assessment);
             }
+
+            List<Enroll> enrolls2 = new ArrayList<>();
+            for (int j = 30; j < 50; j++) {
+                Student student = new Student("student"+j, computer, Long.parseLong(String.valueOf(20240000 + j)));
+                Enroll enroll = new Enroll(student, AI, null);
+                Assessment assessment = new Assessment(j + 80, j + 80, j + 80, enroll);
+                double totalScore = assessment.calculateTotalScore();
+                enrolls2.add(enroll);
+                assessment.changeAssessment(totalScore, enroll);
+                em.persist(student);
+                em.persist(enroll);
+                em.persist(assessment);
+            }
+            Enroll.assignGrade(enrolls2);
             Enroll.assignGrade(enrolls);
 
 
