44bd50bc4e0cc5bc0b4f146836dd8f1f5342c1d1
jdm2131 <jdm2131@naver.com>
2024-06-03 19:02:15 +0900

    feat: 성적 부여 구현

--- src/main/java/Team/project/repository/grade/GradeRepositoryImpl.java
+++ src/main/java/Team/project/repository/grade/GradeRepositoryImpl.java
@@ -1,16 +1,21 @@
 package Team.project.repository.grade;
 
 import Team.project.dto.grade.GradeDto;
+import Team.project.dto.grade.GradeEditDto;
 import Team.project.dto.grade.QGradeDto;
+import Team.project.entity.QAssessment;
 import Team.project.entity.QCourse;
 import Team.project.entity.QEnroll;
 import Team.project.entity.QStudent;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import static Team.project.entity.QAssessment.*;
 import static Team.project.entity.QCourse.*;
 import static Team.project.entity.QEnroll.*;
 import static Team.project.entity.QStudent.*;
@@ -33,11 +38,18 @@ public class GradeRepositoryImpl implements GradeRepositoryCustom{
     public List<GradeDto> findAllGradeByStudentId(Long studentId) {
         return queryFactory
                 .select(new QGradeDto(
-                        student.as("student"),
-                        course.as("course"),
+                        student.id.as("studentId"),
+                        course.id.as("courseId"),
+                        student.name,
+                        course.courseName,
+                        assessment.midTermScore,
+                        assessment.finalTermScore,
+                        assessment.reportScore,
                         enroll.gradeType
                 ))
                 .from(enroll)
+                .join(assessment)
+                .on(enroll.id.eq(assessment.enroll.id))
                 .where(student.id.eq(studentId))
                 .fetch();
     }
@@ -49,12 +61,38 @@ public class GradeRepositoryImpl implements GradeRepositoryCustom{
     public List<GradeDto> findAllGradeByCourseId(Long courseId) {
         return queryFactory
                 .select(new QGradeDto(
-                        student.as("student"),
-                        course.as("course"),
+                        student.id,
+                        course.id,
+                        student.name,
+                        course.courseName,
+                        assessment.midTermScore,
+                        assessment.finalTermScore,
+                        assessment.reportScore,
                         enroll.gradeType
                 ))
                 .from(enroll)
+                .join(assessment)
+                .on(enroll.id.eq(assessment.enroll.id))
                 .where(course.id.eq(courseId))
                 .fetch();
+    }
+
+    @Transactional
+    @Override
+    public void assignGrade(GradeEditDto editDto) {
+        queryFactory
+                .update(assessment)
+                .set(assessment.midTermScore, editDto.getMidTermScore())
+                .set(assessment.finalTermScore, editDto.getFinalTermScore())
+                .set(assessment.reportScore, editDto.getReportScore())
+                .where(assessment.enroll.id.eq(
+                        JPAExpressions.select(enroll.id)
+                                .from(enroll)
+                                .where(enroll.course.id.eq(editDto.getCourseId())
+                                        .and(enroll.student.id.eq(editDto.getStudentId())))
+                ))
+                .execute();
+        em.flush();
+        em.clear();
     }
 }
