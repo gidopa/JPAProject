5188b1c8e7e0c20f0471f5dc1ad140186dd263b4
82109 <kwg08055@naver.com>
2024-06-05 16:55:38 +0900

    feat : 학생, 교수 정보 반환 API 추가

--- src/main/java/Team/project/api/info/ApiInfoController.java
+++ src/main/java/Team/project/api/info/ApiInfoController.java
@@ -1,12 +1,11 @@
 package Team.project.api.info;
 
 import Team.project.dto.info.InfoDto;
-import Team.project.exception.InfoCustomException;
 import Team.project.service.info.InfoService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,38 +16,41 @@ public class ApiInfoController {
 
     private final InfoService infoService;
 
+    /* 학생 정보 반환 */
+    @GetMapping("/student/info/{id}")
+    public ResponseEntity<?> info1(@PathVariable Long id, HttpSession session){
+
+        InfoDto infoDto = infoService.findStudent(id, session);
+
+        return ResponseEntity.ok(infoDto);
+    }
+
+    /* 교수 정보 반환 */
+    @GetMapping("/professor/info/{id}")
+    public ResponseEntity<?> info2(@PathVariable Long id, HttpSession session){
+
+        InfoDto infoDto = infoService.findProfessor(id, session);
+
+        return ResponseEntity.ok(infoDto);
+    }
+
+    /* 학생 정보 수정 */
     @PatchMapping("/student/info/{id}")
     public ResponseEntity<?> update1(@PathVariable Long id, @RequestBody @Valid InfoDto infoDto){
 
-        try{
             InfoDto updateDto = infoService.studentUpdate(id, infoDto);
 
             return ResponseEntity.ok(updateDto);
 
-        }catch (InfoCustomException e){
-            return handlerException(e);
-        }catch (Exception ex){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("에러가 발생하였습니다.");
-        }
-
     }
 
+    /* 교수 정보 수정 */
     @PatchMapping("/professor/info/{id}")
     public ResponseEntity<?> update2(@PathVariable Long id, @RequestBody @Valid InfoDto infoDto){
 
-        try{
             InfoDto updateDto = infoService.professorUpdate(id, infoDto);
 
             return ResponseEntity.ok(updateDto);
-        }catch (InfoCustomException e){
-            return handlerException(e);
-        }catch (Exception ex){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("에러가 발생하였습니다.");
-        }
     }
 
-    @ExceptionHandler(InfoCustomException.class)
-    public ResponseEntity<String> handlerException(InfoCustomException ex){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
-    }
 }
