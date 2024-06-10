e6b33ceb338008e7d0874346c2ac7b9cbcd3c18d
jdm2131 <jdm2131@naver.com>
2024-06-11 01:48:19 +0900

    [fix] : import 구문 추가

--- src/main/java/Team/project/entity/Course.java
+++ src/main/java/Team/project/entity/Course.java
@@ -1,6 +1,7 @@
 package Team.project.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
