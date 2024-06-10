2f31368d4c443a95db3443db47ac24f87185dab1
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-06-10 13:46:05 +0900

    chore : student_history 데이터 삽입 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,17 +2,27 @@
 INSERT INTO Major ( major_id, name) VALUES ( 2,'경영학과'), ( 1,'컴퓨터공학');
 
 -- Student 데이터 삽입
-INSERT INTO student (student_id, hakbun, name, password, major_id, city, street,  years, semester, status)
-VALUES (1, 20000101, 'John Doe', 'password123', 1, 'Cityville', '123 Main St', 2024, 2, 'ENROLLED'),
-       (2, 20000102, 'Kim Wongi', 'password124', 2, 'Cityville', '123 Main St',  2024, 2, 'ENROLLED'),
-       (3, 20240103, 'student3', 'password125', 1, 'Cityville', '123 Main St',  2024, 2, 'ENROLLED'),
-       (4, 20240104, 'student4', 'password125', 1, 'Cityville', '123 Main St',  2024, 2, 'ENROLLED'),
-       (5, 20240105, 'student5', 'password125', 1, 'Cityville', '123 Main St',  2024, 2, 'ENROLLED'),
-       (6, 20240106, 'student6', 'password125', 1, 'Cityville', '123 Main St',  2024, 2, 'ENROLLED'),
-       (7, 20240107, 'student7', 'password125', 1, 'Cityville', '123 Main St',  2024, 2, 'ENROLLED'),
-       (8, 20240108, 'student8', 'password125', 1, 'Cityville', '123 Main St',  2024, 2, 'ENROLLED'),
-       (10, 20240109, 'student10', 'password125', 1, 'Cityville', '123 Main St',  2024, 2, 'ENROLLED'),
-       (9, 20240110, 'student9', 'password125', 1, 'Cityville', '123 Main St',  2024, 2, 'ENROLLED');
+INSERT INTO student (student_id, hakbun, name, password, major_id, city, street,  years, semester)
+VALUES (1, 20000101, 'John Doe', 'password123', 1, 'Cityville', '123 Main St', 2024, 2),
+       (2, 20000102, 'Kim Wongi', 'password124', 2, 'Cityville', '123 Main St',  2024, 2),
+       (3, 20240103, 'student3', 'password125', 1, 'Cityville', '123 Main St',  2024, 2),
+       (4, 20240104, 'student4', 'password125', 1, 'Cityville', '123 Main St',  2024, 2),
+       (5, 20240105, 'student5', 'password125', 1, 'Cityville', '123 Main St',  2024, 2),
+       (6, 20240106, 'student6', 'password125', 1, 'Cityville', '123 Main St',  2024, 2),
+       (7, 20240107, 'student7', 'password125', 1, 'Cityville', '123 Main St',  2024, 2),
+       (8, 20240108, 'student8', 'password125', 1, 'Cityville', '123 Main St',  2024, 2),
+       (10, 20240109, 'student10', 'password125', 1, 'Cityville', '123 Main St',  2024, 2),
+       (9, 20240110, 'student9', 'password125', 1, 'Cityville', '123 Main St',  2024, 2);
+
+-- StudentHistory 데이터 삽입
+INSERT INTO student_history (student_history_id, student_id, old_Status, new_Status)
+VALUES (1, 1, 'ENROLLED', 'ENROLLED'),
+       (2, 2, 'ENROLLED', 'ENROLLED'),
+       (3, 3, 'ENROLLED', 'ENROLLED'),
+       (4, 4, 'ENROLLED', 'ENROLLED'),
+       (5, 5, 'ENROLLED', 'ENROLLED'),
+       (6, 6, 'ENROLLED', 'ONLEAVE'),
+       (7, 1, 'ENROLLED', 'ONLEAVE');
 
 -- Credit 데이터 삽입
 INSERT INTO Credit (credit_id,student_id, credit) VALUES (1, 1,4.0), (2, 2,1.8), (3, 3,2.0);
