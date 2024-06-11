cfb2a3993ac0e13ad4d3ee6dcb74aae1805fb05f
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-06-11 13:34:02 +0900

    fix : 학생 학점 계산 메서드 추가

--- src/main/java/Team/project/entity/Credit.java
+++ src/main/java/Team/project/entity/Credit.java
@@ -1,8 +1,11 @@
 package Team.project.entity;
 
+import Team.project.dto.grade.GradeDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -19,4 +22,31 @@ public class Credit { // 평균 학점
     private Student student;
 
     private float credit; // 평균 학점
+
+    public void addCredit(List<GradeDto> gradeDtos){
+        float sum = 0;
+
+        for (GradeDto gradeDto : gradeDtos) {
+            if(gradeDto.getGradeType().name().equals("APLUS")){
+                sum += 4.5f;
+            }else if(gradeDto.getGradeType().name().equals("AZERO")){
+                sum += 4.0f;
+            }else if(gradeDto.getGradeType().name().equals("BPLUS")){
+                sum += 3.5f;
+            }else if(gradeDto.getGradeType().name().equals("BZERO")){
+                sum += 3.0f;
+            }else if(gradeDto.getGradeType().name().equals("CPLUS")){
+                sum += 2.5f;
+            }else if(gradeDto.getGradeType().name().equals("CZERO")){
+                sum += 2.0f;
+            }else if(gradeDto.getGradeType().name().equals("DPLUS")){
+                sum += 1.5f;
+            }else if(gradeDto.getGradeType().name().equals("DZERO")){
+                sum += 1.0f;
+            }else if(gradeDto.getGradeType().name().equals("F")){
+                sum += 0;
+            }
+        }
+        this.credit = sum / gradeDtos.size();
+    }
 }
